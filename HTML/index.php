94fc40041660d19b3cf2ffdc5a41c2984ecd5b33
Ender Jose Barillas Rodriguez <barillas.ender@gmail.com>
2017-02-08 19:02:26 -0800

    02/08/17

---
+++ HTML/index.php
@@ -0,0 +1,32 @@
+<?php include('includes/header.php') ?>
+
+            <!-- Begin Text -->
+            <article id="text">
+                <h2>Header Level Two</h2>
+                <img src="./images/Ender Barillas.jpg" alt="Ender Barillas" class="align-right size-medium">
+                <p><strong>Hizzle ipsum dolizzle <em>cool</em> amizzle</strong>, consectetuer adipiscing sheezy. Nullizzle sapizzle velizzle, funky fresh volutpat, suscipit izzle, gravida dizzle, funky fresh. Pellentesque phat shizzle my nizzle crocodizzle. Sed erizzle. Mah nizzle izzle i'm in the shizzle you son of a bizzle turpis tempizzle get down get down. Maurizzle shit nibh izzle go to hizzle. Sure check it out tortor. Pellentesque eleifend ghetto daahng dawg. In hac habitasse platea dictumst. Dawg dapibizzle. Yo mamma tellizzle urna, pretium stuff, mattizzle izzle, eleifend vitae, mah nizzle. suscipizzle. Integizzle semper velit sizzle own yo'.</p>
+                <p>Etiam laorizzle urna sure nisl. Sed quis arcu. Maecenizzle pulvinizzle, ipsizzle owned malesuada shit, break yo neck, yall break it down owned tellivizzle, funky fresh luctizzle shut the shizzle up nulla izzle dizzle. Fizzle ullamcorper, tortor izzle varizzle boofron, nibh nunc ultricies go to hizzle, izzle shizzlin dizzle leo ghetto in dolor. Maurizzle ghetto, orci my<em> Helloooooo</em> shizz volutpat consectetuer, sizzle augue luctizzle brizzle, ma nizzle bow wow wow enizzle crackalackin izzle nisl. Nullizzle fo shizzle velizzle bizzle orci eleifend brizzle. Check it out nizzle break yo neck, yall. Shiz non tellivizzle <a href="http://www.sccc.premiumdw.com/"> vel pede pimpin'</a> facilisizzle. Ghetto gizzle nulla, iaculis away, brizzle i saw beyonces tizzles and my pizzle went crizzle, brizzle the bizzle, erizzle. Funky fresh vitae turpis fizzle nibh bibendizzle consequat. Fizzle pulvinar sheezy bow wow wow. Aliquizzle doggy volutpat. Crazy fo shizzle izzle at mammasay mammasa mamma oo sa you son of a bizzle its fo rizzle. Crizzle nec lacus yo dui brizzle ultricies. Ut nisl. Pimpin' et urna. Integizzle laorizzle get down get down shizznit mi. Donec rizzle turpizzle.</p>
+                <h3>Header Level Three</h3>
+                <p class="redtext">Vivamus dawg maurizzle egizzle black away pretium. Vivamus sizzle amizzle crackalackin. Ma nizzle eu fo shizzle mah nizzle fo rizzle, mah home g-dizzle egizzle cool auctizzle feugiat. Praesent black viverra ipsum. Curabitur izzle arcu. Own yo' enizzle ghetto, auctor away, congue eu, dignissim shit, pimpin'. Bizzle sure fo shizzle nizzle crazy posuere stuff. Mofo pede tortor, for sure pulvinar, auctizzle a, ma nizzle sizzle shizznit, its fo rizzle. Doggy at shizzlin dizzle. Aliquizzle fizzle away, elementizzle consectetizzle, sollicitudin izzle, consequizzle , for sure. Quisque nizzle ipsizzle fo shizzle my nizzle mi rutrum vehicula. Tellivizzle accumsizzle mammasay mammasa mamma oo sa ipsum. Pellentesque i saw beyonces tizzles and my pizzle went crizzle fo shizzle my nizzle tristique fo shizzle my nizzle izzle i saw beyonces tizzles and my pizzle went crizzle izzle tellivizzle famizzle izzle turpizzle shizzle my nizzle crocodizzle. In est. Curabitur da bomb. I saw beyonces tizzles and my pizzle went crizzle erizzle felizzle, gangsta quizzle, fo shizzle ac, porta pulvinizzle, crazy. Nulla sure mofo ghetto.</p>
+                <ol>
+                    <li class="redtext">stuff one</li>
+                    <li>stuff two stuff two</li>
+                </ol>
+                <h3>Header Level ThreeX2</h3>
+                <p>Donizzle boom shackalack auctizzle mauris. Phasellus ma nizzle elit ut nibh pretizzle tincidunt. Shut the shizzle up a fo. Vestibulizzle fo shizzle my nizzle shit sed maurizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle dizzle. Fo shizzle my nizzle at uhuh ... yih! sizzle izzle erizzle ultricizzle go to hizzle. In velit ass, ultricizzle at, hendrerit quizzle, adipiscing quis, dui. Etiam velizzle leo, aliquam shiznit, pharetra non, dictizzle sed, turpizzle. Gizzle neque. Crizzle lorizzle. Owned shiznit erat izzle daahng dawg commodo ghetto. Fusce brizzle sheezy pimpin' i saw beyonces tizzles and my pizzle went crizzle ullamcorpizzle break it down. Phat fermentizzle sapien non erizzle. Suspendisse i saw beyonces tizzles and my pizzle went crizzle leo, sollicitudin ma nizzle, dawg izzle, da bomb its fo rizzle, justo. Donizzle funky fresh porttitizzle pizzle. Nunc feugizzle, tellus a ornare gizzle, sapizzle metizzle tincidunt ante, check it out hizzle pede enim ac black. Phasellizzle quam leo, imperdiet ghetto, tempizzle yo mamma, fizzle izzle, shizzle my nizzle crocodizzle. Ut shizznit magna i'm in the shizzle break it down. Yo mamma ante nibh, suscipizzle phat, vestibulizzle izzle, rutrizzle shiznit, ghetto. a mauris. Da bomb magna sit amizzle risus iaculis congue.</p>
+            </article>
+            <!-- End Text -->
+
+            <!-- Begin Sidebar -->
+            <aside id="sidebar">
+                <h2>Page Headline</h2>
+                <ul>
+                    <li>Helloooo222</li>
+                    <li>heeeeeyyy444</li>
+                    <li>3333333</li>
+
+                </ul>
+            </aside>
+            <!-- End Sidebar -->
+
+<?php include('includes/footer.php') ?>
